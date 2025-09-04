2d976178e2e07ece6b7c8896d156d6d102fe69ad
RodrigoDuranAndres <rodrigoduranandres@gmail.com>
2025-09-04 11:37:51 +0200

    prueba3

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "google_storage_bucket" "bucket-1"{
-    name = "github-terraform-bucket-electricity"
-    location = "EU"
-    force_destroy = true
+resource "google_storage_bucket" "bucket-1" {
+  name          = "github-terraform-bucket-electricity"
+  location      = "EU"
+  force_destroy = true
 
-    public_access_prevention = "enforced"
+  public_access_prevention = "enforced"
 }
