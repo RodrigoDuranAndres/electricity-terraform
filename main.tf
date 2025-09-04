6b82a24ffdd2003436477ac6908e0f309e61a916
RodrigoDuranAndres <rodrigoduranandres@gmail.com>
2025-09-04 10:37:45 +0200

    add main terraform

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "bucket-1"{
+    name = "github-terraform-bucket-electricity"
+    location = "EU"
+    force_destroy = true
+
+    public_access_prevention = "enforced"
+}
