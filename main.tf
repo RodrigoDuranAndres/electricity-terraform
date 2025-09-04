41fda3acf52445b8e15719fe75d5ac0ed4513e93
RodrigoDuranAndres <rodrigoduranandres@gmail.com>
2025-09-04 12:18:57 +0200

    prueba4

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "bucket-1" {
   name          = "github-terraform-bucket-electricity"
+  project       = "energia-471108"
   location      = "EU"
   force_destroy = true
 
